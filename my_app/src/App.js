a1dbab223d567e5ed16bb731ff28628b29e9ec27
shane <6874@holbertonstudents.com>
2024-10-25 15:03:26 +0200

    task3

--- my_app/src/App.js
+++ my_app/src/App.js
@@ -1,45 +1,43 @@
-import React, { useState } from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
-// import Input from './components/general/Input.js';
-import SelectInput from './components/general/SelectInput.js';
-import { faUser } from '@fortawesome/free-solid-svg-icons';
-import Button from './components/general/Button';
-import SearchBar from './components/general/SearchBar';
+import axios from 'axios';
 
 function App() {
-  const [selectedOption, setSelectedOption] = useState('');
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const[userUsername, setUserUsername] = useState("");
 
-  // Options à passer au SelectInput
-  const options = [
-    { value: 'Default', label: 'Default' },
-    { value: 'Latest', label: 'Latest' },
-    { value: 'Oldest', label: 'Oldest' },
-    { value: 'Highest Rated', label: 'Highest Rated' },
-    { value: 'Lowest Rated', label: 'Lowest Rated'}
-  ];
-  const handleButtonClick = () => {
-    console.log('Button clicked!'); // Gérer le clic sur le bouton
-  };
-  const [title, setTitle] = useState('');
+  useEffect(() => {
+    // Récupère le token dans le localStorage
+    const accessToken = localStorage.getItem('accessToken');
 
+    // Si le token existe, envoie une requête POST à /api/auth/ avec le token dans le header
+    if (accessToken) {
+      axios.post('/api/auth/', {}, {
+        headers: {
+          Authorization: `Bearer ${accessToken}` // Ajoute le token au header
+        }
+      })
+      .then(response => {
+        // Si la requête réussit, met à jour les états
+        setIsLoggedIn(true); // Indique que l'utilisateur est connecté
+        setUserUsername(response.data.username); // Met à jour le nom d'utilisateur
+      })
+      .catch(error => {
+        console.error('Erreur lors de la vérification de l\'authentification', error);
+        // Si la requête échoue, reste déconnecté
+        setIsLoggedIn(false);
+      });
+    }
+  }, []); // Le tableau vide [] signifie que cet effet ne se déclenche que lors du montage initial
+
+  // Retourne le composant en fonction de l'état de isLoggedIn
   return (
     <div className="App">
-      <SelectInput
-        label="Sort"
-        options={options} // Passer les options ici
-        value={selectedOption} // La valeur actuelle
-        setValue={setSelectedOption} // La fonction pour mettre à jour l'état
-      />
-      <Button
-        label="Load More..."
-        className="my-custom-class"
-        onClick={handleButtonClick}
-        icon={faUser} // Passer l'icône
-      />
-      <SearchBar
-        title={title}
-        setTitle={setTitle}
-      />
+      {isLoggedIn ? (
+        <Dashboard username={userUsername} />
+      ) : (
+        <Authentication />
+      )}
     </div>
   );
 }
